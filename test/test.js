7a51598851f26b1f3f4ecc3e020439e30127fd3d
Justin Stoddard <justinst@USA-IT-B8BG8WP.corp.unicity.net>
2019-03-11 17:39:15 -0600

    added some unit tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,5 @@
 const Type = require('../src');
 
-
 class Kid {
 	constructor () {
 		this.hi = 'hello';
@@ -13,7 +12,9 @@ class Kid {
 var kid1 = new Kid()
 var kid2 = new Kid()
 
-
+var x = process.hrtime()
+var y = process.hrtime()
+// console.log((y[1] - x[1]) / 1000000)
 
 var input = {
 	id : {
@@ -29,7 +30,7 @@ var input = {
 			fourth: [{
 				fail : '1',
 				hi2  : '1',
-				arr  : ['1', '1'],
+				arr  : ['1'],
 				x    : '1'
 			},{
 				fail : '2',
@@ -44,8 +45,7 @@ var input = {
 	kids : [kid1, kid2, 'hi', () => {}]
 };
 
-var x = process.hrtime()
-Type.is(input, {
+Type.assert(input, {
 	id : {
 		hi    : Function,
 		what  : null,
@@ -69,7 +69,113 @@ Type.is(input, {
 	kids : [NaN]
 });
 
-var y = process.hrtime()
-// console.log((y[1] - x[1]) / 1000000)
+//---------------------------------------------Start of Justins Test's
 
-
+const testObjectsArray = [
+	{
+		consoleLog: console.log("Test 1 - Making sure nested types work with Strings in Objects"),
+		testObjectData: {
+			person: {
+				name: "Justin",
+				age: 21,
+				awesome: true,
+				subscribe: () => {},
+				sym: Symbol("hello world"),
+				soul: null,
+				religon: undefined,
+				worth: NaN,
+				hasNo: kid1,
+				or: kid2,
+				table: {},
+				computer: []
+			}
+		},
+		testObjectTypes: {
+			person: {
+				name: String,
+				age: Number,
+				awesome: Boolean,
+				subscribe: Function,
+				sym: Symbol,
+				soul: null,
+				religon: undefined,
+				worth: NaN,
+				hasNo: Kid,
+				or: Kid,
+				table: Object,
+				computer: Array
+			}
+		}
+	},
+	{
+		consoleLog: console.log("Test 2 - Testing nested Arrays inside a parent Object"),
+		testObjectData: {
+			house: {
+				thing: [1,2],
+				otherThing: ['1', '2'],
+				cat: [true, false],
+				in: [null, null],
+				the: [undefined, undefined],
+				hat: [NaN, NaN],
+				cooks: [() => {}, () => {}],
+				good: [[], []],
+				stuff: [{}, {}],
+				moreStuff: [Symbol('hello'), Symbol('there')],
+				evenMoreStuff: [kid1, kid2]
+			}
+		},
+		testObjectTypes: {
+			house: {
+				thing: [Number],
+				otherThing: [String],
+				cat: [Boolean],
+				in: [null],
+				the: [undefined],
+				hat: [NaN],
+				cooks: [Function],
+				good: [Array],
+				stuff: [Object],
+				moreStuff: [Symbol],
+				evenMoreStuff: [Kid]
+			}
+		}
+	},
+	{
+		consoleLog: console.log("Test 3 - Testing Object inside a parent Array"),
+		testObjectData: [
+			{
+				name: "Justin",
+				age: 21,
+				awesome: true,
+				subscribe: () => {},
+				sym: Symbol("hello world"),
+				soul: null,
+				religon: undefined,
+				worth: NaN,
+				hasNo: kid1,
+				or: kid2,
+				table: {},
+				computer: []
+			}
+		],
+		testObjectTypes: [
+			{
+				name: String,
+				age: Number,
+				awesome: Boolean,
+				subscribe: Function,
+				sym: Symbol,
+				soul: null,
+				religon: undefined,
+				worth: NaN,
+				hasNo: Kid,
+				or: Kid,
+				table: Object,
+				computer: Array
+			}
+		]
+	}
+]
+testObjectsArray.map(test => {
+	Type.assert(test.testObjectData, test.testObjectTypes)
+})
